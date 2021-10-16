ec2d505b62956c69b3beb19952cca816082cf003
Saskia Opdam <opdamsaskia@icloud.com>
2021-10-16 16:24:27 +0200

    style

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,7 +10,7 @@ const StyledSection = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  border-bottom: 1px solid cadetblue;
+  border-bottom: 1px solid #eee;
 `;
 
 const SectionHeader = styled.h2`
@@ -19,65 +19,52 @@ const SectionHeader = styled.h2`
   color: cadetblue;
 `;
 
-const ArticleHeader = styled.h3`
-  text-align: center;
-  margin: 30px;
-  color: cadetblue;
-  @media (min-width: 768px) {
-    text-align: left;
-    margin: 0 0 20px 15px;
-  }
-`;
-
 const AboutMe = styled.article`
-  background: ghostwhite;
-  padding: 30px;
   margin-bottom: 20px;
+  background: white;
   /* media queries */
-  @media (min-width: 768px) {
+  @media (min-width: 1000px) {
     display: flex;
-    justify-content: center;
     align-items: stretch;
   }
 `;
 
 const MyPicture = styled.figure`
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  @media (min-width: 768px) {
-    justify-content: flex-end;
-    align-items: center;
+  max-width: 500px;
+  @media (min-width: 1000px) {
     width: 50%;
   }
 `;
 
 const MyAmbition = styled.div`
-  @media (min-width: 768px) {
+  max-width: 500px;
+  @media (min-width: 1000px) {
     width: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
   }
 `;
 
 const Image = styled.img`
   width: 100%;
-  max-width: 430px;
-  @media (min-width: 768px) {
-    margin-right: 15px;
+`;
+
+const Column = styled.div`
+  padding: 20px;
+  @media (min-width: 500px) {
+    padding: 20px 0;
+  }
+  @media (min-width: 1000px) {
+    padding-left: 20px;
   }
 `;
 
 const Paragraph = styled.p`
-  max-width: 430px;
-  margin-bottom: 20px;
   line-height: 1.25;
+  margin-bottom: 20px;
   &:last-child {
-    margin-bottom: 0px;
-  }
-  @media (min-width: 768px) {
-    margin-left: 15px;
-    &:first-child {
-      margin-top: 0px;
-    }
+    margin-bottom: 0;
   }
 `;
 
@@ -106,39 +93,41 @@ function Main() {
             <Image src={image} alt="image of Saskia Opdam" />
           </MyPicture>
           <MyAmbition>
-            <ArticleHeader>Background</ArticleHeader>
-            <Paragraph>
-              While freelancing as a translator and editor I had the opportunity
-              to work on my own website, and a colleagues. This was so inspiring
-              that I kept thinking about it ever since.
-            </Paragraph>
-            <Paragraph>
-              This year I decided to take the plunge and start a new adventure.
-              In February I enrolled in a full-stack development course at Winc
-              Academy (remote learning). So far I have gained knowledge and
-              experience in HTML, CSS, SCSS, JavaScript, React, Redux, Jest,
-              Git, Github and NodeJS.
-            </Paragraph>
-            <Paragraph>
-              At present I'm busy with the final front-end assignment: a
-              react-app showing ratings in a chart (student-dashboard). When
-              finished, I will proceed with the back-end development course,
-              which will cover Python, Flask and SQL.
-            </Paragraph>
-            <Paragraph>
-              What I love about programming is that it allows for creativity and
-              logical thinking, at the same time, both of which I love. It would
-              be great to gain experience and broaden my front-end knowledge in
-              a junior developer job, while in the spare time finishing the
-              back-end course.
-            </Paragraph>
-            <Paragraph>
-              So, if you are looking for an enthusiastic beginner programmer
-              with a creative background (academy of arts), who loves to learn
-              and would bring a problem solving attitude to your team, please
-              feel free to get in touch.
-            </Paragraph>
-            <Paragraph>Hope to hear from you soon!</Paragraph>
+            {/* <ArticleHeader>Background</ArticleHeader> */}
+            <Column>
+              <Paragraph>
+                While freelancing as a translator and editor I had the
+                opportunity to work on my own website, and a colleagues. This
+                was so inspiring that I kept thinking about it ever since.
+              </Paragraph>
+              <Paragraph>
+                This year I decided to take the plunge and start a new
+                adventure. In February I enrolled in a full-stack development
+                course at Winc Academy (remote learning). So far I have gained
+                knowledge and experience in HTML, CSS, SCSS, JavaScript, React,
+                Redux, Jest, Git, Github and NodeJS.
+              </Paragraph>
+              <Paragraph>
+                At present I'm busy with the final front-end assignment: a
+                react-app showing ratings in a chart (student-dashboard). When
+                finished, I will proceed with the back-end development course,
+                which will cover Python, Flask and SQL.
+              </Paragraph>
+              <Paragraph>
+                What I love about programming is that it allows for creativity
+                and logical thinking, at the same time, both of which I love. It
+                would be great to gain experience and broaden my front-end
+                knowledge in a junior developer job, while in the spare time
+                finishing the back-end course.
+              </Paragraph>
+              <Paragraph>
+                So, if you are looking for an enthusiastic beginner programmer
+                with a creative background (academy of arts), who loves to learn
+                and would bring a problem solving attitude to your team, please
+                feel free to get in touch.
+              </Paragraph>
+              <Paragraph>Hope to hear from you soon!</Paragraph>
+            </Column>
           </MyAmbition>
         </AboutMe>
       </StyledSection>
