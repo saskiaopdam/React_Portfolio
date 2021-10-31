e283f236245bf3f8b8449bec8c7bb40f702d70a3
Saskia Opdam <opdamsaskia@icloud.com>
2021-10-31 14:45:26 +0100

    user friendly design

--- src/App.js
+++ src/App.js
@@ -3,13 +3,58 @@ import { BrowserRouter as Router } from "react-router-dom";
 import styled from "styled-components/macro";
 
 import ScrollToTop from "./components/ScrollToTop";
-import Navigation from "./components/Navigation/Navigation";
-import Content from "./components/Content/Content";
+import Navbar from "./components/Navbar";
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
   min-height: 100vh;
+`;
+
+const Header = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
   background: black;
+  color: white;
+  font-size: 12px;
+  // box-shadow: 0px 0 5px rgba(0, 0, 0, 0.8);
+`;
+
+const Main = styled.main`
+  flex-grow: 1;
+  display: flex;
+  margin-top: 58px;
+`;
+
+const Footer = styled.footer`
+  flex-shrink: 0;
+  background: black;
+  color: white;
+  font-size: 12px;
+  // box-shadow: 0 -0px 5px rgba(0, 0, 0, 0.8);
+`;
+
+const Inner = styled.div`
+  flex-grow: 1;
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 20px;
+  &.headerInner {
+    padding: 10px;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 40px;
+  margin-bottom: 28px;
+`;
+
+const Subtitle = styled.h2`
+  font-size: 24px;
+  margin-bottom: 20px;
+  color: rebeccapurple;
 `;
 
 function App() {
@@ -17,8 +62,81 @@ function App() {
     <Wrapper>
       <Router>
         <ScrollToTop>
-          <Navigation />
-          <Content />
+          <Header>
+            <Inner className="headerInner">
+              <Navbar />
+            </Inner>
+          </Header>
+          <Main>
+            <Inner>
+              <Title>
+                I'm a junior front-end developer learning full-stack
+              </Title>
+              <p>I know a bit about:</p>
+              <Subtitle>
+                HTML | CSS | JavaScript | React | Redux &amp; NodeJS
+              </Subtitle>
+              <p>I'm about to learn:</p>
+              <Subtitle>Python | Flask &amp; SQL</Subtitle>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </Inner>
+          </Main>
+          <Footer>
+            <Inner>© 2021 Saskia Opdam | build with React</Inner>
+          </Footer>
         </ScrollToTop>
       </Router>
     </Wrapper>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+import styled from "styled-components/macro";
+
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+const Wrapper = styled.nav`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Brand = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const BurgerMenu = styled.div`
+  display: flex;
+  @media (min-width: 500px) {
+    display: none;
+  }
+  background: black;
+`;
+
+const Burger = styled.button`
+  display: flex;
+  align-items: center;
+  background: black;
+  color: white;
+  border: 1px solid transparent;
+  font-size: 16px;
+  padding: 10px;
+`;
+
+const BurgerLabel = styled.span`
+  font-size: 12px;
+  margin-right: 6px;
+`;
+
+const FullMenu = styled.div`
+  display: none;
+  @media (min-width: 500px) {
+    display: flex;
+  }
+  // background: blue;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: white;
+  padding: 10px;
+
+  &.selected {
+    pointer-events: none;
+    text-decoration: underline;
+  }
+`;
+
+function NavBar() {
+  const [open, setOpen] = useState(false);
+  console.log(`open: ${open}`);
+
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <Wrapper>
+      <Brand>
+        <StyledNavLink to="/home">SASKIA OPDAM</StyledNavLink>
+      </Brand>
+      <Menu>
+        <BurgerMenu
+          open={open}
+          setOpen={setOpen}
+          onClick={toggleMenu}
+          aria-label={open ? "close the menu" : "open the menu"}
+        >
+          <Burger>
+            <BurgerLabel>{open ? "sluit" : "menu"}</BurgerLabel>
+            {open ? <AiOutlineClose /> : <AiOutlineMenu />}
+          </Burger>
+        </BurgerMenu>
+        <FullMenu>
+          <StyledNavLink to="/about" activeClassName="selected">
+            about
+          </StyledNavLink>
+          <StyledNavLink to="/work" activeClassName="selected">
+            work
+          </StyledNavLink>
+          <StyledNavLink to="/contact" activeClassName="selected">
+            contact
+          </StyledNavLink>
+        </FullMenu>
+      </Menu>
+    </Wrapper>
+  );
+}
+
+export default NavBar;
