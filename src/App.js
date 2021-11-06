533117fad086daccf8b4a59e5d70b5010e1f0139
Saskia Opdam <opdamsaskia@icloud.com>
2021-11-06 16:33:11 +0100

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Hashrouter as Router, Route } from "react-router-dom";
 import { useState } from "react";
 
 import styled from "styled-components/macro";
