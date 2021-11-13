04cb6453eae0b7de7a3f3c687a597241f8d06e74
Saskia Opdam <opdamsaskia@icloud.com>
2021-11-13 17:38:37 +0100

    changed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import styled from "styled-components/macro";
 
@@ -9,26 +9,12 @@ const Wrapper = styled.nav`
   justify-content: space-between;
 `;
 
-const StyledLink = styled(Link)`
-  color: black;
-  text-decoration: none;
-  font-size: 14px;
-  & + & {
-    margin-left: 14px;
-  }
-  &.selected {
-    pointer-events: none;
-    font-weight: bold;
-  }
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
 const Info = styled.div``;
 
 const Name = styled.h1`
-  font-size: 22px;
+  font-size: 18px;
+  font-weight: 500;
+  block-size: fit-content;
 `;
 
 const Role = styled.h2`
@@ -40,32 +26,24 @@ const Location = styled.p`
   font-weight: 500;
 `;
 
-const BurgerMenu = styled.button`
+const Burger = styled.button`
+  flex-grow: 1;
+  height: 21px;
   display: flex;
   justify-content: flex-end;
-  background: transparent;
+  align-items: center;
   border: none;
-  font-size: 20px;
-  padding-left: 44px;
+  font-size: 18px;
   @media (min-width: 500px) {
     display: none;
   }
 `;
 
-const Icon = styled.div`
-  height: 26px;
-  display: flex;
-  align-items: center;
-`;
-
 const FullMenu = styled.div`
   display: none;
-  // display: flex;
-  // flex-direction: column;
-  // align-items: flex-end;
-  @media (min-width: 768px) {
+  @media (min-width: 500px) {
     display: flex;
-    flex-direction: row;
+    align-items: center;
   }
 `;
 
@@ -73,9 +51,10 @@ const StyledNavLink = styled(NavLink)`
   color: black;
   text-decoration: none;
   font-size: 14px;
-  // & + & {
-  //   margin-left: 14px;
-  // }
+  block-size: fit-content;
+  & + & {
+    margin-left: 14px;
+  }
   &.selected {
     pointer-events: none;
     font-weight: bold;
@@ -92,6 +71,8 @@ const AnchorLink = styled.a`
   color: black;
   text-decoration: none;
   font-size: 14px;
+  block-size: fit-content;
+  margin-left: 14px;
   &:hover {
     text-decoration: underline;
   }
@@ -107,41 +88,35 @@ function NavBar({ open, setOpen, toggleMenu }) {
         className="homelink"
       >
         <Name>Saskia Opdam</Name>
-        <Role>Junior front-end developer</Role>
-        <Location>The Hague</Location>
       </StyledNavLink>
-      <BurgerMenu
+      {/* <Role>Junior front-end developer</Role>
+        <Location>The Hague</Location> */}
+      {/* </StyledNavLink> */}
+      <Burger
         open={open}
         setOpen={setOpen}
         onClick={toggleMenu}
         aria-label={open ? "close the menu" : "open the menu"}
       >
-        {open ? (
-          <Icon>
-            <AiOutlineClose />
-          </Icon>
-        ) : (
-          <Icon>
-            <AiOutlineMenu />
-          </Icon>
-        )}
-      </BurgerMenu>
+        {open ? <AiOutlineClose /> : <AiOutlineMenu />}
+      </Burger>
+
       <FullMenu>
         <StyledNavLink exact to="/" activeClassName="selected">
-          Home
+          home
         </StyledNavLink>
         <StyledNavLink to="/about" activeClassName="selected">
-          About
+          about
         </StyledNavLink>
         <StyledNavLink to="/work" activeClassName="selected">
-          Work
+          work
         </StyledNavLink>
         <AnchorLink
           href="https://www.behance.net/opdamsaski0505"
           target="_blank"
           rel="noreferrer"
         >
-          Hobby
+          hobby
         </AnchorLink>
       </FullMenu>
     </Wrapper>
